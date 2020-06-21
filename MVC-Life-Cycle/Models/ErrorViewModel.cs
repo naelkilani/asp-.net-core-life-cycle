082b818fe542a70da57d01254211011b40fc3729
Nael Kilani <naelkilanii@gmail.com>
2020-06-21 17:20:26 +0300

    setup project.

---
+++ MVC-Life-Cycle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Life_Cycle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
