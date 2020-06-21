dca44c077ace4826c46acc051383ac34f363ef8b
Nael Kilani <naelkilanii@gmail.com>
2020-06-21 17:32:49 +0300

    setup database.

--- MVC-Life-Cycle/Data/ApplicationDbContext.cs
+++ MVC-Life-Cycle/Data/ApplicationDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +9,45 @@ namespace MVC_Life_Cycle.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>(entity => entity.Property
+                    (p => p.Id).HasMaxLength(128));
+            builder.Entity<IdentityRole>(entity => entity.Property
+                    (p => p.NormalizedName).HasMaxLength(128));
+
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property
+                    (p => p.LoginProvider).HasMaxLength(128));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property
+                    (p => p.UserId).HasMaxLength(128));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property
+                    (p => p.Name).HasMaxLength(128));
+
+            builder.Entity<IdentityUserRole<string>>(entity => entity.Property
+                    (p => p.UserId).HasMaxLength(128));
+            builder.Entity<IdentityUserRole<string>>(entity => entity.Property
+                    (p => p.RoleId).HasMaxLength(128));
+
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property
+                    (p => p.LoginProvider).HasMaxLength(128));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property
+                    (p => p.ProviderKey).HasMaxLength(128));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property
+                    (p => p.UserId).HasMaxLength(128));
+
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.Property
+                    (p => p.Id).HasMaxLength(128));
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.Property
+                    (p => p.UserId).HasMaxLength(128));
+
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property
+                    (p => p.Id).HasMaxLength(128));
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property
+                    (p => p.RoleId).HasMaxLength(128));
         }
     }
 }
--- MVC-Life-Cycle/Startup.cs
+++ MVC-Life-Cycle/Startup.cs
@@ -28,7 +28,7 @@ namespace MVC_Life_Cycle
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseMySQL(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
